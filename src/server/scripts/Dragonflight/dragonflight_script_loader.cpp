48c9406de1f7af2ba644e2b44099c9436930df32
fuzionproject <75014882+fuzionproject@users.noreply.github.com>
2022-09-21 18:53:16 +0300

    some changes

--- src/server/scripts/Dragonflight/dragonflight_script_loader.cpp
+++ src/server/scripts/Dragonflight/dragonflight_script_loader.cpp
@@ -16,166 +16,109 @@
  */
 
 //Zones
-void AddSC_zone_exiles_reach();
-void AddSC_zone_bastion();
-void AddSC_zone_the_maw();
-void AddSC_zone_Kortia();
-void AddSC_zone_Sanctum_Of_Domiantion();
-void AddSC_zone_oribos();
-void AddSC_zone_maldraxxus();
-void AddSC_zone_ardenweald();
-void AddSC_zone_revendreth(); 
-//Darkmaul Citadel
-void AddSC_instance_darkmaul_citadel();
-void AddSC_darkmaul_citadel();
-void AddSC_boss_tunk();
-void AddSC_boss_gorgroth();
-void AddSC_boss_ravnyr();
-//ShadowlandsIntro
-void AddSC_ShadowlandsIntro();
-//Necrotic Wake
-void AddSC_instance_necrotic_wake();
-void AddSC_necrotic_wake();
-void AddSC_boss_blightbone();
-void AddSC_boss_amarth();
-void AddSC_boss_surgeon_stitchflesh();
-void AddSC_boss_nalthor();
-//Plaguefall
-void AddSC_instance_plaguefall();
-void AddSC_boss_globgrog();
-void AddSC_boss_doctor_ickus();
-void AddSC_boss_domina_venomblade();
-void AddSC_boss_margrave_stradama();
-//Spires of Ascension
-void AddSC_instance_spires_of_ascension();
-void AddSC_boss_kintara();
-void AddSC_boss_ventunax();
-void AddSC_boss_oryphrion();
-void AddSC_boss_devos();
-//Castle Nathria
-void AddSC_instance_castle_nathria();
-void AddSC_zone_castle_nathria();
-void AddSC_boss_shriekwing();
-void AddSC_boss_huntsman_altimor();
-void AddSC_boss_hungering_destroyer();
-void AddSC_boss_lady_inerva_darkvein();
-void AddSC_boss_artificer_xymox();
-void AddSC_boss_sun_kings_salvation();
-void AddSC_boss_council_of_blood();
-void AddSC_boss_sludgefist();
-void AddSC_boss_stone_legion_generals();
-void AddSC_boss_sire_denathrius();
-//De Other Side
-void AddSC_instance_de_other_side();
-void AddSC_boss_hakkar_the_soulflayer_os();
-void AddSC_boss_the_manastorms();
-void AddSC_boss_dealer_xyexa();
-void AddSC_boss_muehzala();
-//Mists of Tirna Scithe
-void AddSC_instance_mists_of_tirna_scithe();
-void AddSC_mist_of_tirna_scithe();
-//Sanguine Depths
-void AddSC_instance_sanquine_depths();
-//Halls of Atonement
-void AddSC_instance_halls_of_attonement();
-//Sanctum of Domination
-void AddSC_instance_Sanctum_of_Domination();
-void AddSC_Sanctum_of_Domination();
-//Shackles of Fate
-void AddSC_Boss_Fatescriber_Roh_Kalo();
-void AddSC_Boss_Guardian_of_the_First_Ones();
-void AddSC_Boss_Kel_Thuzad();
-//The Dark Bastille
-void AddSC_Boss_Painsmith_Raznal();
-void AddSC_Boss_Remnant_of_Ner_zhul();
-void AddSC_Boss_Soulrender_Dormazain();
-//The Jailer's Vanguard
-void AddSC_Boss_Eye_of_the_Jailer();
-void AddSC_Boss_The_Nine();
-void AddSC_Boss_The_Tarragrue();
-//The Reckoning
-void AddSC_Boss_Sylvanas_Windrunner();
-//Shadowland
-void AddShadowlandsScripts()
+void AddSC_Zone_Ohnahran_Plains();
+void AddSC_Zone_Valdrakken();
+//
+void AddSC_Zone_Thaldraszus();
+void AddSC_Zone_Valdrakken();
+//
+void AddSC_Zone_TheAzureSpan();
+void AddSC_Zone_Vault_of_the_Incarnates();
+void AddSC_Zone_BrackenhideHollow();
+
+//dungeons:
+
+// Bracken hide Hollow
+void AddSC_instance_BrackenhideHollow();
+
+//Halls Of Infusion
+void AddSC_instance_HallsOfInfusion();
+void AddSC_Boss_Watcher_Irideus();
+//Neltharus
+void AddSC_Instance_Neltharus();
+//Ruby Life Pools
+void AddSC_Instance_RubyLifePools();
+//Tazalgeth Academy
+void AddSC_Instance_TazalgethAcademy();
+void AddSC_TazalgethAcademy();
+void AddSC_Hungry_Lasher();
+void AddSC_Ancient_Branch();
+void AddSC_Boss_Crawth();
+void AddSC_Boss_Echo_of_Doragosa();
+void AddSC_Boss_Overgrown_Ancient();
+void AddSC_Boss_Vexamus();
+//The Azure Vault
+void AddSC_Instance_TheAzureVault();
+//The Nokhud Offensive
+void AddSC_Instance_TheNokhudOffensive();
+//Uldaman Legacy Of Tyr
+void AddSC_Instance_UldamanLegacyOfTyr();
+//raid
+
+//Vault_of_the_Incarnates
+void AddSC_Instance_Vault_of_the_Incarnates();
+void AddSC_Zone_Vault_of_the_Incarnates();
+void AddSC_Boss_Broodkeeper_Diurna();
+void AddSC_Boss_Dathea_Ascended();
+void AddSC_Boss_Eranog();
+void AddSC_Boss_Kurog_Grimtotem();
+void AddSC_Boss_Raszageth_the_Storm_Eater();
+void AddSC_Boss_Sennarth_The_Cold_Breath();
+void AddSC_Boss_Terros();
+void AddSC_Boss_The_Primalist_Council();
+//dragonflight
+void AddDragonflightScripts()
 {
     //Zones
-    AddSC_zone_exiles_reach();
-    AddSC_zone_bastion();
-    AddSC_zone_the_maw();
-    AddSC_zone_Kortia();
-    AddSC_zone_oribos();
-    AddSC_zone_maldraxxus();
-    AddSC_zone_ardenweald();
-    AddSC_zone_revendreth();
-    //Darkmaul Citadel
-    AddSC_instance_darkmaul_citadel();
-    AddSC_darkmaul_citadel();
-    AddSC_boss_tunk();
-    AddSC_boss_gorgroth();
-    AddSC_boss_ravnyr();
-    //ShadowlandsIntro
-    AddSC_ShadowlandsIntro();
-    //Necrotic Wake
-    AddSC_instance_necrotic_wake();
-    AddSC_necrotic_wake();
-    AddSC_boss_blightbone();
-    AddSC_boss_amarth();
-    AddSC_boss_surgeon_stitchflesh();
-    AddSC_boss_nalthor();
-    //Plaguefall
-    AddSC_instance_plaguefall();
-    AddSC_boss_globgrog();
-    AddSC_boss_doctor_ickus();
-    AddSC_boss_domina_venomblade();
-    AddSC_boss_margrave_stradama();
-    //Spires of Ascension
-    AddSC_instance_spires_of_ascension();
-    AddSC_boss_kintara();
-    AddSC_boss_ventunax();
-    AddSC_boss_oryphrion();
-    AddSC_boss_devos();
-    //Castle Nathria
-    AddSC_instance_castle_nathria();
-    AddSC_zone_castle_nathria();
-    AddSC_boss_shriekwing();
-    AddSC_boss_huntsman_altimor();
-    AddSC_boss_hungering_destroyer();
-    AddSC_boss_lady_inerva_darkvein();
-    AddSC_boss_artificer_xymox();
-    AddSC_boss_sun_kings_salvation();
-    AddSC_boss_council_of_blood();
-    AddSC_boss_sludgefist();
-    AddSC_boss_stone_legion_generals();
-    AddSC_boss_sire_denathrius();
-    //De Other Side
-    AddSC_instance_de_other_side();
-    AddSC_boss_hakkar_the_soulflayer_os();
-    AddSC_boss_the_manastorms();
-    AddSC_boss_dealer_xyexa();
-    AddSC_boss_muehzala();
-    //Mists of Tirna Scithe
-    AddSC_instance_mists_of_tirna_scithe();
-    //Sanguine Depths
-    AddSC_instance_sanquine_depths();
-    //Halls of Atonement
-    AddSC_instance_halls_of_attonement();
-    //Sanctum of Domination
-    AddSC_instance_Sanctum_of_Domination();
-    AddSC_Sanctum_of_Domination();
-    //Shackles of Fate
-    AddSC_Boss_Fatescriber_Roh_Kalo();
-    AddSC_Boss_Guardian_of_the_First_Ones();
-    AddSC_Boss_Kel_Thuzad();
-    //The Dark Bastille
-    AddSC_Boss_Painsmith_Raznal();
-    AddSC_Boss_Remnant_of_Ner_zhul();
-    AddSC_Boss_Soulrender_Dormazain();
-    //The Jailer's Vanguard
-    AddSC_Boss_Eye_of_the_Jailer();
-    AddSC_Boss_The_Nine();
-    AddSC_Boss_The_Tarragrue();
-    //The Reckoning
-    AddSC_Boss_Sylvanas_Windrunner();
-    //Shadowland
-    AddShadowlandsScripts();
+    AddSC_Zone_Ohnahran_Plains();
+    //
+    AddSC_Zone_Thaldraszus();
+    AddSC_Zone_Valdrakken();
+    //
+    AddSC_Zone_TheAzureSpan();
+    AddSC_Zone_Vault_of_the_Incarnates();
+    AddSC_Zone_BrackenhideHollow();
+
+    //dungeons:
+
+    // Bracken hide Hollow
+    AddSC_instance_BrackenhideHollow();
+
+    //Halls Of Infusion
+    AddSC_instance_HallsOfInfusion();
+    AddSC_Boss_Watcher_Irideus();
+    //Neltharus
+    AddSC_Instance_Neltharus();
+    //Ruby Life Pools
+    AddSC_Instance_RubyLifePools();
+    //Tazalgeth Academy
+    AddSC_Instance_TazalgethAcademy();
+    AddSC_TazalgethAcademy();
+    AddSC_Hungry_Lasher();
+    AddSC_Ancient_Branch();
+    AddSC_Boss_Crawth();
+    AddSC_Boss_Echo_of_Doragosa();
+    AddSC_Boss_Overgrown_Ancient();
+    AddSC_Boss_Vexamus();
+    //The Azure Vault
+    AddSC_Instance_TheAzureVault();
+    //The Nokhud Offensive
+    AddSC_Instance_TheNokhudOffensive();
+    //Uldaman Legacy Of Tyr
+    AddSC_Instance_UldamanLegacyOfTyr();
+    //raid
+
+    //Vault_of_the_Incarnates
+    AddSC_Instance_Vault_of_the_Incarnates();
+    AddSC_Zone_Vault_of_the_Incarnates();
+    AddSC_Boss_Broodkeeper_Diurna();
+    AddSC_Boss_Dathea_Ascended();
+    AddSC_Boss_Eranog();
+    AddSC_Boss_Kurog_Grimtotem();
+    AddSC_Boss_Raszageth_the_Storm_Eater();
+    AddSC_Boss_Sennarth_The_Cold_Breath();
+    AddSC_Boss_Terros();
+    AddSC_Boss_The_Primalist_Council();
+    //Dragonflight
+    AddDragonflightScripts();
 };
