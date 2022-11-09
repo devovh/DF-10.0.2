ddb963bf389b83532963d3f519749f1afc8e3910
fuzionproject <75014882+fuzionproject@users.noreply.github.com>
2022-11-09 13:19:12 +0200

    implement all dungeons bosses!

--- src/server/scripts/Dragonflight/dragonflight_script_loader.cpp
+++ src/server/scripts/Dragonflight/dragonflight_script_loader.cpp
@@ -30,14 +30,32 @@ void AddSC_Zone_BrackenhideHollow();
 
 // Bracken hide Hollow
 void AddSC_instance_BrackenhideHollow();
+void AddSC_Zone_BrackenhideHollow();
+void AddSC_Boss_Rira_Hackclaw();
+void AddSC_Boss_Treemouth();
+void AddSC_Boss_Gutshot();
+void AddSC_Boss_Decatriarch_Wratheye();
 
 //Halls Of Infusion
 void AddSC_instance_HallsOfInfusion();
 void AddSC_Boss_Watcher_Irideus();
+void AddSC_Boss_Curious_Swoglet();
+void AddSC_Boss_Khajin_the_Unyielding();
+void AddSC_Boss_Primal_Tsunami();
+
 //Neltharus
 void AddSC_Instance_Neltharus();
+void AddSC_Boss_Chargath_Bane_of_Scales();
+void AddSC_Boss_Forgemaster_Gorek();
+void AddSC_Boss_Magmatusk();
+void AddSC_Boss_Warlord_Sargha();
+
 //Ruby Life Pools
 void AddSC_Instance_RubyLifePools();
+void AddSC_Boss_Melidrussa_Chillworn();
+void AddSC_Boss_Kokia_Blazehoof();
+void AddSC_Boss_Kyrakka();
+
 //Tazalgeth Academy
 void AddSC_Instance_TazalgethAcademy();
 void AddSC_TazalgethAcademy();
@@ -47,12 +65,29 @@ void AddSC_Boss_Crawth();
 void AddSC_Boss_Echo_of_Doragosa();
 void AddSC_Boss_Overgrown_Ancient();
 void AddSC_Boss_Vexamus();
+
 //The Azure Vault
 void AddSC_Instance_TheAzureVault();
+void AddSC_Boss_Leymor();
+void AddSC_Boss_Azureblade();
+void AddSC_Boss_Telash_Greywing();
+void AddSC_Boss_Umbrelskul();
+
 //The Nokhud Offensive
 void AddSC_Instance_TheNokhudOffensive();
+void AddSC_Boss_Granyth();
+void AddSC_Boss_The_Raging_Tempest();
+void AddSC_Boss_Teera_and_Maruuk();
+void AddSC_Boss_Balakar_Khan();
+
 //Uldaman Legacy Of Tyr
 void AddSC_Instance_UldamanLegacyOfTyr();
+void AddSC_Boss_The_Lost_Dwarves();
+void AddSC_Boss_Bromach();
+void AddSC_Boss_Sentinel_Talondras();
+void AddSC_Boss_Emberon();
+void AddSC_Boss_Chrono_Lord_Deios();
+
 //raid
 
 //Vault_of_the_Incarnates
@@ -83,14 +118,32 @@ void AddDragonflightScripts()
 
     // Bracken hide Hollow
     AddSC_instance_BrackenhideHollow();
+    AddSC_Zone_BrackenhideHollow();
+    AddSC_Boss_Rira_Hackclaw();
+    AddSC_Boss_Treemouth();
+    AddSC_Boss_Gutshot();
+    AddSC_Boss_Decatriarch_Wratheye();
 
     //Halls Of Infusion
     AddSC_instance_HallsOfInfusion();
     AddSC_Boss_Watcher_Irideus();
+    AddSC_Boss_Curious_Swoglet();
+    AddSC_Boss_Khajin_the_Unyielding();
+    AddSC_Boss_Primal_Tsunami();
+
     //Neltharus
     AddSC_Instance_Neltharus();
+    AddSC_Boss_Chargath_Bane_of_Scales();
+    AddSC_Boss_Forgemaster_Gorek();
+    AddSC_Boss_Magmatusk();
+    AddSC_Boss_Warlord_Sargha();
+
     //Ruby Life Pools
     AddSC_Instance_RubyLifePools();
+    AddSC_Boss_Melidrussa_Chillworn();
+    AddSC_Boss_Kokia_Blazehoof();
+    AddSC_Boss_Kyrakka();
+
     //Tazalgeth Academy
     AddSC_Instance_TazalgethAcademy();
     AddSC_TazalgethAcademy();
@@ -100,12 +153,29 @@ void AddDragonflightScripts()
     AddSC_Boss_Echo_of_Doragosa();
     AddSC_Boss_Overgrown_Ancient();
     AddSC_Boss_Vexamus();
+
     //The Azure Vault
     AddSC_Instance_TheAzureVault();
+    AddSC_Boss_Leymor();
+    AddSC_Boss_Azureblade();
+    AddSC_Boss_Telash_Greywing();
+    AddSC_Boss_Umbrelskul();
+
     //The Nokhud Offensive
     AddSC_Instance_TheNokhudOffensive();
+    AddSC_Boss_Granyth();
+    AddSC_Boss_The_Raging_Tempest();
+    AddSC_Boss_Teera_and_Maruuk();
+    AddSC_Boss_Balakar_Khan();
+
     //Uldaman Legacy Of Tyr
     AddSC_Instance_UldamanLegacyOfTyr();
+    AddSC_Boss_The_Lost_Dwarves();
+    AddSC_Boss_Bromach();
+    AddSC_Boss_Sentinel_Talondras();
+    AddSC_Boss_Emberon();
+    AddSC_Boss_Chrono_Lord_Deios();
+
     //raid
 
     //Vault_of_the_Incarnates
